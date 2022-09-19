b792c671c6debae484a0297a04914e470895f11c
Just Morsh <prateeksingh2001@gmail.com>
2022-09-19 16:41:06 +0530

    new 	new file:   C6 prac q4.py

---
+++ C6 prac q4.py
@@ -0,0 +1,7 @@
+a = input("Enter a username: ")
+b = len(a)
+print("length of the input username is: ", b)
+if b < 10:
+    print("Length of the username is less then 10 characters")
+else:
+    print("Length of the username is more then 10 characters")
